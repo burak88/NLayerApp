4a24519d91f5809ef65d3b234bc79a467bdf2112
burak88 <burakilic43@gmail.com>
2022-03-03 11:47:27 +0300

    Add project files.

---
+++ NLayer.Service/Services/CategoryService.cs
@@ -0,0 +1,32 @@
+﻿using AutoMapper;
+using NLayer.Core;
+using NLayer.Core.DTOs;
+using NLayer.Core.Repositories;
+using NLayer.Core.Services;
+using NLayer.Core.UnitOfWorks;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NLayer.Service.Services
+{
+    public class CategoryService : Service<Category>, ICategoryService
+    {
+        private readonly ICategoryRepository _categoryReposityory;
+        private readonly IMapper _mapper;
+        public CategoryService(IGenericRepository<Category> repository, IUnitOfWork unitOfWork, IMapper mapper, ICategoryRepository categoryReposityory) : base(repository, unitOfWork)
+        {
+            _mapper = mapper;
+            _categoryReposityory = categoryReposityory;
+        }
+
+        public async Task<CustomResponseDto<CategoryWithProductDto>> GetSingleCategoryByIdWithProductsAsync(int categoryId)
+        {
+            var category = await _categoryReposityory.GetSingleCategoryByIdWithProductsAsync(categoryId);
+            var categoryDto = _mapper.Map<CategoryWithProductDto>(category);
+            return CustomResponseDto<CategoryWithProductDto>.Success(200, categoryDto);
+        }
+    }
+}
